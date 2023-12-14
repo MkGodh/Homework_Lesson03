ca69e9b2bff5fd6ef61b7f9118ae30c998e7c218
MkGodh <fenryrvla@yandex.ru>
2023-12-14 18:28:08 +0300

    Initial commit

---
+++ Exmple_01/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+using System;
+class IntArray
+{
+    static void Main()
+{
+
+  int[] array = new int[10];
+  Random nums = new Random();
+  for (int i = 0; i < array.Length; i++)
+{
+ array[i] = nums.Next(20, 90);
+ Console.Write("* " + array[i] + " ");
+}
+Console.Write("! ");
+}
+
+}
---
+++ Exmple_03/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+
+using System;
+
+class Program
+{
+  static void Main()
+  {
+        Random rnd = new Random();
+        double[] array = Enumerable.Range(0, 10).Select(x => rnd.NextDouble() * 10 - 5).ToArray();
+        
+        double minValue = array.Min();
+        double maxValue = array.Max();
+
+        double difference = maxValue - minValue;
+
+        Console.WriteLine("Мин. элемент: " + minValue);
+        Console.WriteLine("Макс. элемент: " + maxValue);
+        Console.WriteLine("Разница: " + difference);
+  }
+}
